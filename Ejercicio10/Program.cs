ee6ba1241369524a194cab7a8d38a3c569cde3f3
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-08 22:13:35 -0500

    ini

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,31 @@
+﻿//Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor,
+//el del medio y el menor de los números ingresados.
+
+namespace ejercicio10
+{
+    // 1 definir clase y metodo
+
+    public class program
+    {
+        static void Main(string[] Args)
+        {
+            //2 difinir variables
+            double num1, num2, num3, mayor, menor, medio;
+
+            //3 pedir datos en pantalla
+            Console.WriteLine("Digite el primer numero: ");
+            num1 = Convert.ToDouble(Console.Read());
+            Console.WriteLine("Digite el segundo numero: ");
+            num2 = Convert.ToDouble(Console.Read());
+            Console.WriteLine("Digite el tercer numero: ");
+            num3 = Convert.ToDouble(Console.Read());
+
+            // 4 calculos
+
+            mayor = Math.Max(num1, Math.Max(num2, num3));
+
+
+
+        }
+    }
+}
